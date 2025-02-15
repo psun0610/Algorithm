cd00bbf20d75db906dade23104b5c8bc83e98f91
SunYoung <psy010610@naver.com>
2025-02-15 23:23:54 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,9 +1,5 @@
 function solution(a, d, included) {
-    let sum_ = 0
-    for(let i=0; i<included.length; i++) {
-        if(included[i]) {
-            sum_ += (a+d*i)
-        }
-    }
-    return sum_;
+    return included.reduce((acc, cur, i) => {
+        return cur ? acc + a + d * i : acc
+    }, 0)
 }
