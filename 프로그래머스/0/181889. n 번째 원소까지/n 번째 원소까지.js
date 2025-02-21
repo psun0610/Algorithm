48b1c7de77245268ff5b23d02fc16513c0bf55ee
SunYoung <psy010610@naver.com>
2025-02-21 21:26:33 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,9 +1,3 @@
 function solution(num_list, n) {
-    let new_list = [];
-    
-    for(let i=0; i<n; i++) {
-        new_list.push(num_list[i]);
-    }
-    
-    return new_list;
+    return num_list.slice(0, n);
 }
