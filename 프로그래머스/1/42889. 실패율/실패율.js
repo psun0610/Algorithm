c8e656dcee94e949bf19015e5c2b41ff81fc6ddd
SunYoung <psy010610@naver.com>
2025-02-27 18:43:34 +0900

    [level 1] Title: 실패율, Time: 5162.02 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -5,26 +5,15 @@
     실패율이 높은 스테이지부터 내림차순으로 스테이지 번호 배열 return
 */
 function solution(N, stages) {
-    // [0]은 도달한 수
-    // [1]은 클리어하지 못한 플레이어 수
-    let allStages = new Array(N).fill(0).map(() => new Array(2).fill(0));
+    let answer = [];
     
-    for(let i=0; i<stages.length; i++) {
-        for(let j=0; j<Math.min(stages[i], N); j++) {
-            allStages[j][0]++;
-            if(j == stages[i]-1) {
-                allStages[j][1]++;
-            }
-        }
+    for(let i=1; i<=N; i++) {
+        const all = stages.filter(x => x >= i).length;
+        const fail = stages.filter(x => x === i).length;
+        
+        answer.push([i, fail/all]);
     }
     
-    let failureRates = allStages.map((stage, index) => {
-        if (stage[0] === 0) {
-            return { stage: index + 1, rate: 0 };
-        }
-        return { stage: index + 1, rate: stage[1] / stage[0] };
-    });
-    
-    failureRates.sort((a, b) => b.rate - a.rate);
-    return failureRates.map((item) => item.stage);
+    answer.sort((a, b) => b[1] - a[1]);
+    return answer.map(item => item[0]);
 }
