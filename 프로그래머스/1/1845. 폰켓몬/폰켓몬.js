4199ff058fbaac609c718f6e241abb41c04ad4dd
SunYoung <psy010610@naver.com>
2025-03-06 14:03:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,3 @@
 function solution(nums) {
-    var dict = {};
-    
-    nums.map(num => {
-        dict[num] ? dict[num]++ : dict[num] = 1;
-    })
-    
-    return Math.min(nums.length/2, Object.keys(dict).length);
+    return Math.min(nums.length/2, new Set(nums).size);
 }
